44bcbb98809921f8d22bf269504d453b1873579b
Christopher Turner <turner296@gmail.com>
2011-07-28 15:57:37 +0100

    changed indentation

--- ci-f/helpers/fdatabase_helper.php
+++ ci-f/helpers/fdatabase_helper.php
@@ -4,15 +4,15 @@ if (!function_exists('fDatabase')) {
     function fDatabase ($type, $database=null, $user=null, $pass=null, $host=null, $port=null, $time=null)
     {
         if (true === $type) {
-    		if (!defined('ENVIRONMENT') || !file_exists($file_path = APPPATH.'config/'.ENVIRONMENT.'/database'.EXT)) {
-    			if (!file_exists($file_path = APPPATH.'config/database'.EXT)) {
-    				throw new fUnexpectedException('Could not locate database configurations.');
-    			}
-    		}
-    		
-    		include($file_path);
-    		
-    		$type     = $db[$active_group]['dbdriver'];
+            if (!defined('ENVIRONMENT') || !file_exists($file_path = APPPATH.'config/'.ENVIRONMENT.'/database'.EXT)) {
+                if (!file_exists($file_path = APPPATH.'config/database'.EXT)) {
+                    throw new fUnexpectedException('Could not locate database configurations.');
+                }
+            }
+            
+            include($file_path);
+            
+            $type     = $db[$active_group]['dbdriver'];
             $database = $db[$active_group]['database'];
             $user     = $db[$active_group]['username'];
             $pass     = $db[$active_group]['password'];
